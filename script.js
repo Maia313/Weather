a4d0f5bf45eb2164d454e47df5a3adb3c11592d6
maia <medeea@rocketmail.com>
2018-03-08 18:07:32 +0100

    update script.js

--- script.js
+++ script.js
@@ -3,32 +3,25 @@ const proxy = "https://crossorigin.me/";
 const dsAPI = "https://api.darksky.net/forecast/7535a35daa4bf7d1231073bae0263e0c/";
 const options = "?units=auto&extend=hourly";
 
-var geolocation;
-var weather;
 var hourlyWeatherHeights;
 
 document.addEventListener('DOMContentLoaded', init());
 
 function init() {
-    $.getJSON(ipinfo, function(geolocationData) {
-        console.log(geolocationData);
-        geolocation = geolocationData;
-        //initGeolocationDiv();
-
-        $.getJSON(proxy + dsAPI + getLatLong() + options, function(weatherData) {
-            weather = weatherData;
-            initNowDiv();
-            initWeatherDailyDiv();
-            initWeatherHourlyDiv();
-            console.log(weather);
+    fetch(ipinfo)
+        .then(geolocationData => {
+            return fetch(proxy + dsAPI + getLatLong(geolocationData) + options);
+        })
+        .then(weatherData => {
+            initNowDiv(weatherData);
+            initWeatherDailyDiv(weatherData);
+            initWeatherHourlyDiv(weatherData);
+            console.log(weatherData);
         });
-
-
-    });
 }
 
 
-function getLatLong() {
+function getLatLong(geolocation) {
     if (!geolocation) {
         console.error("No GeoLocation Data");
     }
@@ -37,33 +30,34 @@ function getLatLong() {
 
 
 //Weather Now
-function initNowDiv() {
+function initNowDiv(weatherData) {
 
-    $("#now").html(
+    document.getElementById("#now").innerHTML(
         "<br><h2> Now </h2>" +
-        "<h3>" + round(weather.hourly.data[0].temperature, 0) + "&deg" + "</h3><br>" +
+        "<h3>" + round(weatherData.hourly.data[0].temperature, 0) + "&deg" + "</h3><br>" +
         "<img src='img/clouds.svg'>" +
-        "<h3>" + weather.currently.summary + "</h3>"
+        "<h3>" + weatherData.currently.summary + "</h3>"
     );
 }
 
 
 //Weather Daily 
-function initWeatherDailyDiv() {
+function initWeatherDailyDiv(weatherData) {
     for (var i = 0; i < 7; i++) {
-        renderWeatherDayDiv(i);
+        renderWeatherDayDiv(i, weatherData);
     }
     initWeatherDailyEventListener();
 }
 
-function renderWeatherDayDiv(index) {
-    var tempMax = round(weather.daily.data[index].apparentTemperatureMax, 0);
-    var tempMin = round(weather.daily.data[index].apparentTemperatureMin, 0);
-    var descrip = weather.daily.data[index].icon;
-    var ms = weather.daily.data[index].time;
+function renderWeatherDayDiv(index, weatherData) {
+    var tempMax = round(weatherData.daily.data[index].apparentTemperatureMax, 0);
+    var tempMin = round(weatherData.daily.data[index].apparentTemperatureMin, 0);
+    var descrip = weatherData.daily.data[index].icon;
+    var ms = weatherData.daily.data[index].time;
     var day = getDayOfWeek(ms);
 
-    $("#day" + index).html(
+    var daydiv = document.getElementById("#day");
+    (daydiv + index).innerHTML(
         "<h2>" + day + "</h2><br>" +
         "<h3>" + tempMax + "&deg / " + tempMin + "&deg" + "</h3>" +
         getWeatherImg() +
@@ -79,17 +73,23 @@ function renderWeatherDayDiv(index) {
             return "<img src='img/clouds.svg'>";
         else if (descrip == "snow")
             return "<img src='img/snow.svg'>";
-        else if (descrip == "sunny")
+        else if (descrip == "clear-day")
             return "<img src='img/sunny.svg'>";
+        else if (descrip == "wind")
+            return "<img src='img/wind.svg'>";
+        else if (descrip == "wind")
+            return "<img src='img/flag.svg'>";
+        else if (descrip == "fog")
+            return "<img src='img/flag.svg'>";
     }
 
 }
 
 // add event listeners to all 7 weather-days
-function initWeatherDailyEventListener() {
+function initWeatherDailyEventListener(daydiv) {
     for (var i = 0; i < 7; i++) {
         const j = i;
-        $("#day" + j).on("click", function() {
+        (daydiv + j).addEventListener("click", function() {
             select(j);
         });
     }
@@ -97,23 +97,23 @@ function initWeatherDailyEventListener() {
 
 
 // Unselect a weather-day div and then select a weather-day div
-function select(index) {
+function select(index, daydiv) {
     console.log("select(" + index + ")");
-    $(".selected").removeClass("selected");
-    $("#day" + index).addClass("selected");
+    document.getElementsByClassName(".selected").className.remove("selected");
+    (daydiv + index).className.add("selected");
     renderWeatherHourlyDiv(index);
 }
 
 //Weather Hourly
 
-function initWeatherHourlyDiv() {
+function initWeatherHourlyDiv(weatherData) {
     var tempMax = Number.MIN_VALUE;
     var tempMin = Number.MAX_VALUE;
 
     hourlyWeatherHeights = new Array(168);
 
     for (var i = 0; i < 168; i++) {
-        var temp = weather.hourly.data[i].temperature;
+        var temp = weatherData.hourly.data[i].temperature;
         if (temp > tempMax) {
             tempMax = temp;
         }
@@ -121,8 +121,8 @@ function initWeatherHourlyDiv() {
             tempMin = temp;
         }
     }
-    for (var i = 0; i < 168; i++) {
-        temp = weather.hourly.data[i].temperature;
+    for (i = 0; i < 168; i++) {
+        temp = weatherData.hourly.data[i].temperature;
         hourlyWeatherHeights[i] = 120 * (temp - tempMin) / (tempMax - tempMin);
     }
     select(0);
@@ -135,11 +135,12 @@ function renderWeatherHourlyDiv(index) {
     }
 }
 
-function renderGraphColumn(colIndex, index) {
-    var temp = round(weather.hourly.data[index].temperature, 0);
-    var time = getHour(weather.hourly.data[index].time);
+function renderGraphColumn(colIndex, index, weatherData) {
+    var temp = round(weatherData.hourly.data[index].temperature, 0);
+    var time = getHour(weatherData.hourly.data[index].time);
     var height = hourlyWeatherHeights[index];
-    $("#col" + colIndex).html(
+    var column = document.getElementById("#col");
+    (column + colIndex).innerHTML(
         "<p>" + temp + "</p>" +
         " <div class='bar shadow' style='height:" + height + "'></div>" +
         "<p>" + time + "<br>"
@@ -177,16 +178,16 @@ function getDayOfWeek(time) {
 
     var date = new Date(time * 1000);
     var day = date.getDay();
-    var dateNr = date.getDate();
+
 
     var month = date.getMonth();
     var today = (new Date).getDay();
 
     if (day == today) {
-        return "Today, " + "<br>" + "<h4>" + dateNr + " " + monNames[month] + "</h4>";
+        return "Today, " + "<br>" + "<h4>" + day + " " + monNames[month] + "</h4>";
 
     }
-    return weekdays[day] + "," + "<br>" + "<h4>" + dateNr + " " + monNames[month] + "</h4>";
+    return weekdays[day] + "," + "<br>" + "<h4>" + day + " " + monNames[month] + "</h4>";
 
 }
 
@@ -200,11 +201,11 @@ function getHour(time) {
     return time;
 }
 
-function getDayIndex(index) {
+function getDayIndex(index, weatherData) {
     if (index === 0) {
         return 0;
     }
-    var dt = new Date(weather.hourly.data[index * 24].time);
+    var dt = new Date(weatherData.hourly.data[index * 24].time);
     var hour = dt.getHours() - 1;
 
     return index * 24 - hour;
