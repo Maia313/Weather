8f404537feb9f193a335dab5c1730d5d80378b1e
Maia <28684401+Maia313@users.noreply.github.com>
2018-03-12 11:50:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -82,8 +82,11 @@ function renderWeatherDayDiv(index) {
             return "<img src='img/clouds.svg'>";
         else if (descrip == "snow")
             return "<img src='img/snow.svg'>";
-        else if (descrip == "sunny")
+        else if (descrip == "clear")
             return "<img src='img/sunny.svg'>";
+        else if (descrip == "fog")
+            return "<img src='img/cloud.svg'>";
+        
     }
 
 }
